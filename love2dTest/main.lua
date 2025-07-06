985a3a1fd00ea02c061ce9cbba0502a672b07240
Duggu@1308 <work.vaibhav1308@gmail.com>
2025-07-06 09:23:30 +0000

    player movments and animations

--- love2dTest/main.lua
+++ love2dTest/main.lua
@@ -1,5 +1,7 @@
 
 function love.load()
+    anim8 = require 'libraries/anim8'
+    
     -- Get the desktop dimensions (1 for primary monitor)
     local width, height = love.window.getDesktopDimensions(1)
     
@@ -17,28 +19,57 @@ function love.load()
     player = {}
     player.x = 400
     player.y = 500
-    player.speed = 5
-    player.sprite = love.graphics.newImage("objects/Teacher.png")
-    background = love.graphics.newImage("objects/classroom.png")
+    player.walkingSpeed = 2
+    player.animSpeed = 0.1
+    player.spriteSheet = love.graphics.newImage("sprites/Teacher_1_walk-Sheet.png")  
+    background = love.graphics.newImage("sprites/classroom.png")
+
+    player.grid = anim8.newGrid(32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+
+    player.animations= {}
+    player.animations.down = anim8.newAnimation(player.grid('1-6', 4), player.animSpeed)
+    player.animations.up = anim8.newAnimation(player.grid('1-6', 2), player.animSpeed)
+    player.animations.left = anim8.newAnimation(player.grid('1-6', 1), player.animSpeed)
+    player.animations.right = anim8.newAnimation(player.grid('1-6', 3), player.animSpeed)
+
+    player.anim = player.animations.down
+
 end
 
 function love.update(dt)
+    
+    local isMoving = false
+
     if love.keyboard.isDown('w') then
-        player.y = player.y - player.speed
+        player.y = player.y - player.walkingSpeed
+        player.anim = player.animations.up
+        isMoving = true
     end
     if love.keyboard.isDown('s') then
-        player.y = player.y + player.speed
+        player.y = player.y + player.walkingSpeed
+        player.anim = player.animations.down
+        isMoving = true
     end
     if love.keyboard.isDown('d') then
-        player.x = player.x + player.speed
+        player.x = player.x + player.walkingSpeed
+        player.anim = player.animations.right
+        isMoving = true
     end
     if love.keyboard.isDown('a') then
-        player.x = player.x - player.speed
+        player.x = player.x - player.walkingSpeed
+        player.anim = player.animations.left
+        isMoving = true
     end
+
+    if isMoving == false then
+        player.anim:gotoFrame(1)
+    end
+    
+    player.anim:update(dt)
 end
 
 function love.draw()
     -- Draw something on the screen
-    love.graphics.draw(background, 0, 0, nil, 3, 3)
-    love.graphics.draw(player.sprite, player.x, player.y, nil, 3, 3)
+    love.graphics.draw(background, 0, 0, nil, 2, 2)
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3, 3)
 end
