e3d456b7760d35892eae794c647438e8b4f78fcc
Duggu1308 <work.vaibhav1308@gmail.com>
2025-09-13 20:33:15 +0530

    Added camera tracking and small bug fixex

--- love2dTest/main.lua
+++ love2dTest/main.lua
@@ -1,13 +1,18 @@
-
 function love.load()
     anim8 = require 'libraries/anim8'
     
-    -- Get the desktop dimensions (1 for primary monitor)
+    sti = require 'libraries/sti'
+    gameMap = sti('maps/tileSet.lua')
+
+    camera = require 'libraries/camera'
+    cam = camera()
+    
+    -- Get the desktop dimensions (1wad for primary monitor)
     local width, height = love.window.getDesktopDimensions(1)
     
     -- Set the window size to 80% of desktop dimensions
-    local windowWidth = math.floor(width * 0.8)
-    local windowHeight = math.floor(height * 0.8)
+    local windowWidth = math.floor(width * 0.9)
+    local windowHeight = math.floor(height * 0.9)
     love.window.setMode(windowWidth, windowHeight, {
         resizable = true,
         fullscreen = false
@@ -15,14 +20,18 @@ function love.load()
 
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    cam.scale = 4
+
     -- Player object
     player = {}
-    player.x = 400
-    player.y = 500
-    player.walkingSpeed = 2
+    -- Set the player's starting position to the center of the first tile
+    player.x = gameMap.width
+    player.y = gameMap.height 
+    -- Player object
+
+    player.walkingSpeed = 1.5
     player.animSpeed = 0.1
     player.spriteSheet = love.graphics.newImage("sprites/Teacher_1_walk-Sheet.png")  
-    background = love.graphics.newImage("sprites/classroom.png")
 
     player.grid = anim8.newGrid(32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
@@ -33,13 +42,11 @@ function love.load()
     player.animations.right = anim8.newAnimation(player.grid('1-6', 3), player.animSpeed)
 
     player.anim = player.animations.down
-
 end
 
 function love.update(dt)
     
     local isMoving = false
-
     if love.keyboard.isDown('w') then
         player.y = player.y - player.walkingSpeed
         player.anim = player.animations.up
@@ -66,10 +73,41 @@ function love.update(dt)
     end
     
     player.anim:update(dt)
+    cam:lookAt(player.x, player.y)
+
+    local windowWidth = love.graphics.getWidth()
+    local windowHeight = love.graphics.getHeight()
+
+    local mapWidth = gameMap.width * gameMap.tilewidth
+    local mapHeight = gameMap.height * gameMap.tileheight
+
+    if cam.x < (windowWidth / (2 * cam.scale)) then 
+        cam.x = (windowWidth / (2 * cam.scale))
+    end
+
+    if cam.y < (windowHeight / (2 * cam.scale)) then 
+        cam.y = (windowHeight / (2 * cam.scale))
+    end
+
+    if cam.x > (mapWidth - (windowWidth / (2 * cam.scale))) then 
+        cam.x = (mapWidth - (windowWidth / (2 * cam.scale)))
+    end
+
+    if cam.y > (mapHeight - (windowHeight / (2 * cam.scale))) then 
+        cam.y = (mapHeight - (windowHeight / (2 * cam.scale)))
+    end
 end
 
+
+
 function love.draw()
-    -- Draw something on the screen
-    love.graphics.draw(background, 0, 0, nil, 2, 2)
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3, 3)
+    cam:attach() -- Pass the scale factor to the camera
+        gameMap:drawLayer(gameMap.layers['Base Floor'])
+        gameMap:drawLayer(gameMap.layers['Base Stage Floor'])
+        gameMap:drawLayer(gameMap.layers['Floor and Wall Objects'])
+        gameMap:drawLayer(gameMap.layers['Base Wall'])
+        gameMap:drawLayer(gameMap.layers['Wall Objects'])
+        gameMap:drawLayer(gameMap.layers['Benches and Vegetation'])
+        player.anim:draw(player.spriteSheet, player.x, player.y, nil, cam.scale / 3) -- Draw at normal size
+    cam:detach()
 end
