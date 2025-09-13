5ec787324fc99f446f7eb33316f138e0f8f6fcd5
Duggu1308 <work.vaibhav1308@gmail.com>
2025-09-14 00:33:13 +0530

    Added camera tracking and small bug fixex

--- love2dTest/main.lua
+++ love2dTest/main.lua
@@ -7,6 +7,10 @@ function love.load()
     camera = require 'libraries/camera'
     cam = camera()
     
+    wf = require 'libraries/windfield'
+
+    world = wf.newWorld(0, 0)
+
     -- Get the desktop dimensions (1wad for primary monitor)
     local width, height = love.window.getDesktopDimensions(1)
     
@@ -24,15 +28,17 @@ function love.load()
 
     -- Player object
     player = {}
+    player.collider = world:newBSGRectangleCollider(100, 100, 18, 35, 4)
+    player.collider:setFixedRotation(true)
     -- Set the player's starting position to the center of the first tile
-    player.x = gameMap.width
-    player.y = gameMap.height 
-    -- Player object
+    player.x = 400
+    player.y = 200
 
-    player.walkingSpeed = 1.5
+    -- Player object
+    player.walkingSpeed = 50
     player.animSpeed = 0.1
     player.spriteSheet = love.graphics.newImage("sprites/Teacher_1_walk-Sheet.png")  
-
+    
     player.grid = anim8.newGrid(32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 
     player.animations= {}
@@ -42,37 +48,57 @@ function love.load()
     player.animations.right = anim8.newAnimation(player.grid('1-6', 3), player.animSpeed)
 
     player.anim = player.animations.down
+    walls = {}
+
+    if gameMap.layers["Walls"] then
+        for i, obj in pairs(gameMap.layers["Walls"].objects) do 
+            local  wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            wall:setType("static")
+            table.insert(walls, wall)
+        end
+    end
 end
 
 function love.update(dt)
     
     local isMoving = false
+
+    local vx = 0
+    local vy = 0
+    
     if love.keyboard.isDown('w') then
-        player.y = player.y - player.walkingSpeed
+        vy = player.walkingSpeed * -1
         player.anim = player.animations.up
         isMoving = true
     end
     if love.keyboard.isDown('s') then
-        player.y = player.y + player.walkingSpeed
+        vy = player.walkingSpeed
         player.anim = player.animations.down
         isMoving = true
     end
     if love.keyboard.isDown('d') then
-        player.x = player.x + player.walkingSpeed
+        vx = player.walkingSpeed
         player.anim = player.animations.right
         isMoving = true
     end
     if love.keyboard.isDown('a') then
-        player.x = player.x - player.walkingSpeed
+        vx = player.walkingSpeed * -1
         player.anim = player.animations.left
         isMoving = true
     end
+
+    player.collider:setLinearVelocity(vx, vy)
 
     if isMoving == false then
         player.anim:gotoFrame(1)
     end
     
+    world:update(dt)
+    player.x = player.collider:getX() + 2 -- Manual Adjusting offset of Collider
+    player.y = player.collider:getY()
+
     player.anim:update(dt)
+
     cam:lookAt(player.x, player.y)
 
     local windowWidth = love.graphics.getWidth()
@@ -106,8 +132,16 @@ function love.draw()
         gameMap:drawLayer(gameMap.layers['Base Stage Floor'])
         gameMap:drawLayer(gameMap.layers['Floor and Wall Objects'])
         gameMap:drawLayer(gameMap.layers['Base Wall'])
+        gameMap:drawLayer(gameMap.layers['Windows'])
         gameMap:drawLayer(gameMap.layers['Wall Objects'])
         gameMap:drawLayer(gameMap.layers['Benches and Vegetation'])
-        player.anim:draw(player.spriteSheet, player.x, player.y, nil, cam.scale / 3) -- Draw at normal size
+        player.anim:draw(
+            player.spriteSheet,
+            player.x, player.y,
+            nil,
+            cam.scale / 3,
+            cam.scale / 3,
+            16, 16
+        )
     cam:detach()
 end
